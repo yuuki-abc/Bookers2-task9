5fdcd7fa540a7b740af8549b9e1ce8debd1fbb1c
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-06 17:40:01 +0000

    最終チェック終了

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   get 'search' => 'search#search'
-  # get 'chat/show/:id/'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :users, only: [:show, :index, :edit, :update] do
     resource :relationships, only: [:index, :create, :destroy]
-    resources :chat, only: [:show, :update, :destroy]
+    resources :chat, only: [:show, :update, :destroy] # 質問 resourceに出来ないのか？
     get 'follows', 'followers'
   end
   resources :books do
