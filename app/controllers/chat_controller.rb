61261c869fd5f6f7c4ca22b0a99025a8d81762c7
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-06 13:23:21 +0000

    一度urlを見直す

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -7,7 +7,7 @@ class ChatController < ApplicationController
   end
 
   def update
-    Chat.create(message: params[:chat][:message], user_id: current_user.id, room_id: @my_user_room.room_id)
+    Chat.create(message: params[:chat][:message], user_id: current_user.id, room_id: @room)
     @new_chat = Chat.new
     render 'chat_add'
   end
@@ -20,21 +20,19 @@ class ChatController < ApplicationController
 
     def chats_maker
       trigger = true
-      if !!@my_user_room = UserRoom.find_by(user_id: current_user.id) # 少なくとも誰かとチャットした事がある場合
-        if !!@you_user_room = UserRoom.find_by(user_id: params[:user_id]) # 少なくとも相手が誰かとチャットした事がある場合
-          if !!@my_user_room = UserRoom.find_by(room_id: @you_user_room.room_id) # 相手と会話した事があったなら
-            room = @my_user_room.room_id & @you_user_room.room_id
-            @chats = Chat.where(room_id: room)
+      if UserRoom.find_by(user_id: current_user.id) # 誰かとチャットした事がある?
+        if you_user_room = UserRoom.find_by(user_id: params[:user_id]) # 相手が誰かとチャットした事がある?
+          if my_user_room = UserRoom.find_by(room_id: you_user_room.room_id) # 相手と会話した事がある?
+            @room = my_user_room.room_id & you_user_room.room_id
+            @chats = Chat.where(room_id: @room)
             trigger = false
-            puts "\n\n\nチャット経験者\n\n\n"
           end
         end
       end
       if trigger
-        puts "\n\n\n初めての人\n\n\n"
         @room = Room.create
-        @you_user_room = UserRoom.create(room_id: @room.id, user_id: params[:user_id])
-        @my_user_room = UserRoom.create(room_id: @room.id, user_id: current_user.id)
+        UserRoom.create(room_id: @room.id, user_id: params[:user_id])
+        UserRoom.create(room_id: @room.id, user_id: current_user.id)
       end
     end
 
